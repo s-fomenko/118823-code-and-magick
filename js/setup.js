4e4bc48972b7cd4d615ccb43ce94183574fc4464
Sergey Fomenko <fomenko.personal@gmail.com>
2019-01-08 22:09:56 +0300

    created createMockData function

--- js/setup.js
+++ js/setup.js
@@ -2,3 +2,59 @@
 
 var setup = document.querySelector('.setup');
 setup.classList.remove('hidden');
+
+var createMockData = function () {
+  var mockData = [];
+
+  var firstNameList = [
+    'Иван',
+    'Хуан Себастьян',
+    'Мария',
+    'Кристоф',
+    'Виктор',
+    'Юлия',
+    'Люпита',
+    'Вашингтон'
+  ];
+
+  var lastNameList = [
+    'да Марья',
+    'Верон',
+    'Мирабелла',
+    'Вальц',
+    'Онопко',
+    'Топольницкая',
+    'Нионго',
+    'Ирвинг'
+  ];
+
+  var coatColorList = [
+    'rgb(101, 137, 164)',
+    'rgb(241, 43, 107)',
+    'rgb(146, 100, 161)',
+    'rgb(56, 159, 117)',
+    'rgb(215, 210, 55)',
+    'rgb(0, 0, 0)'
+  ];
+
+  var eyesColorList = [
+    'black',
+    'red',
+    'blue',
+    'yellow',
+    'green'
+  ];
+
+  for (var i = 0; i < 4; i++) {
+    var dataItem = {};
+
+    dataItem.name = firstNameList[Math.floor(Math.random() * firstNameList.length)] + ' ' + lastNameList[Math.floor(Math.random() * lastNameList.length)];
+    dataItem.coatColor = coatColorList[Math.floor(Math.random() * coatColorList.length)];
+    dataItem.eyesColor = eyesColorList[Math.floor(Math.random() * eyesColorList.length)];
+
+    mockData.push(dataItem);
+  }
+  return mockData;
+};
+
+console.log(createMockData());
